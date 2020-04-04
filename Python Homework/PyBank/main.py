ebf008d55db4aa5fb0301d7fd99e5c16eff88962
Chad Smith <chadsmith@Chads-MacBook-Pro.local>
2020-04-04 15:16:37 -0700

    Homework Add

--- Python Homework/PyBank/main.py
+++ Python Homework/PyBank/main.py
@@ -1,17 +1,36 @@
 import os
 import csv
-
+import numpy as np
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
 with open(csvpath) as csvfile:
 	csvreader = csv.reader(csvfile)
 
 	csv_header = next(csvreader)
-	counter = 0
-	total_number = 0
+	line1 = next(csvreader)
+	counter = 1
+	total_number = int(line1[1])
+	(csv_header[0])
+	previous_data = int(line1[1])
+	changes = []
+	months = []
 	for row in csvreader:
+		if counter != 0:
+			changes.append(int(row[1]) - int(previous_data))
+			months.append(row[0])
+			previous_data = int(row[1]) 
+
+			
 		counter += 1
 		total_number = int(row[1]) + total_number
-	print(counter)
-	print(total_number)
+	dictionary = dict(zip(changes, months))
+	(dictionary.get(np.max(changes)))
+	(dictionary.get(np.min(changes)))
 
+	print('FINANCIAL ANALYSIS')
+	print('-----------------------------')
+	print('Total Months: ' + str(counter))
+	print('Total: $' + str(total_number))
+	print('Average Change: $' + str(round(np.mean(changes), 2)))
+	print('Greatest Increase in Profits: ' + str(dictionary.get(np.max(changes))) + ' ($' + str(np.max(changes)) + ')')
+	print('Greatest Decrease in Profits: ' + str(dictionary.get(np.min(changes))) + ' ($' + str(np.min(changes)) + ')')
--- Python Homework/PyPoll/main.py
+++ Python Homework/PyPoll/main.py
@@ -1,11 +1,21 @@
 import os
 import csv
 
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'election_data.csv')
 
-with open(csvpath, newline='') as csvfile:
+with open(csvpath) as csvfile:
+		csvreader = csv.reader(csvfile)
 
-	csvreader = csv.reader(csvfile, delimiter = ',')
+		csv_header = next(csvreader)
+		
+		counter = 0
 
-	for row in csvreader:
-		print(row)
+
+		for row in csvreader:
+			counter += 1
+
+		
+
+
+
+		print('Total Votes: ' + str(counter))
